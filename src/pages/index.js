f9c88724daa43960a523e7de0085513bc57b0eee
Riley Marcum <rmarcum13@gmail.com>
2024-06-13 21:35:00 -0400

    create api data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,6 @@
 import "../pages/index.css";
+
+const owner = "aivaxela";
+const repo = "beach-bandits";
+const path = "src/shared-data/address.txt";
+const api = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
